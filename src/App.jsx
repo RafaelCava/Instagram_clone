f8085bab0bc050627695e59c9f8836e8efd352fc
Rafael <ra.facavalcante@hotmail.com>
2021-08-14 20:07:41 -0300

    inserindo eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import Header from './components/Header'
-import Post from './components/Post'
+import Header from './components/Header';
+import Post from './components/Post';
 import { db, auth } from './firebase';
 
 function App() {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    auth.onAuthStateChanged(val => {
-      setUser(val.displayName)
-    })
+    auth.onAuthStateChanged((val) => {
+      setUser(val.displayName);
+    });
 
     db.collection('posts').orderBy('timestamp', 'desc').onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((document) => {
-        return {
-          id: document.id,
-          info: document.data()
-        }
-      }))
-    })
-
-  }, [])
+      setPosts(snapshot.docs.map((document) => ({
+        id: document.id,
+        info: document.data(),
+      })));
+    });
+  }, []);
 
   return (
     <div className="App">
-      <Header setUser={setUser} user={user}></Header>
+      <Header setUser={setUser} user={user} />
       {
-        posts.map(val => {
-          return (
-            <>
-              <Post user={user} info={val.info} id={val.id} />
-            </>
-          )
-        })
+        posts.map((val) => (
+          <>
+            <Post user={user} info={val.info} id={val.id} />
+          </>
+        ))
       }
     </div>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,59 @@
-import { db } from '../firebase'
 import { useState, useEffect } from 'react';
+import { db } from '../firebase';
+
 function Post(props) {
+  const [comentarios, setComentarios] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').doc(props.id).collection('comentarios').onSnapshot((snapshot) => {
+      setComentarios(snapshot.docs.map((document) => ({
+        id: document.id,
+        info: document.data(),
+      })));
+    });
+  }, []);
 
   function comentar(id, e) {
     e.preventDefault();
 
-    let comentarioAtual = document.querySelector('#comentario-'+id).value
-    
+    const comentarioAtual = document.querySelector(`#comentario-${id}`).value;
 
     db.collection('posts').doc(id).collection('comentarios').add({
       nome: props.user,
-      comentario: comentarioAtual
-    })
+      comentario: comentarioAtual,
+    });
 
-    document.querySelector('#comentario-'+id).value = ''
+    document.querySelector(`#comentario-${id}`).value = '';
   }
-
 
   return (
     <div className="postSingle">
       <img src={props.info.image} alt="imagem-postada" />
-      <p><b>{props.info.userName}</b>:{props.info.titulo}</p>
-      <form onSubmit={e => comentar(props.id, e)}>
-        <textarea id={'comentario-'+props.id}></textarea>
+      <p>
+        <b>{props.info.userName}</b>
+        :
+        {props.info.titulo}
+      </p>
+      <div className="coments">
+        {
+          comentarios.map((val) => (
+            <div className="coment-single">
+              <p>
+                <b>{val.info.nome}</b>
+                :
+                {' '}
+                {val.info.comentario}
+              </p>
+            </div>
+          ))
+        }
+      </div>
+      <form onSubmit={(e) => comentar(props.id, e)}>
+        <textarea id={`comentario-${props.id}`} />
         <input type="submit" value="Comentar" />
       </form>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
